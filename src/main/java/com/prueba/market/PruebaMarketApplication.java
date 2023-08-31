9edf73cbeaad30e8bdf264b0f41e7e3fa518a4f9
ingjaz <ingjaz_236@hotmail.com>
2023-08-31 12:07:46 -0600

    Initial commit

---
+++ src/main/java/com/prueba/market/PruebaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaMarketApplication.class, args);
+	}
+
+}
